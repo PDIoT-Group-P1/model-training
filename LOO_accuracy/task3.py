8c5022699c2d418ba5c8132637f25f02a469d82a
PC53 <s2104454@ed.ac.uk>
2024-01-15 12:48:28 +0530

    task 3 script done (model updating required)

--- LOO_accuracy/task3.py
+++ LOO_accuracy/task3.py
@@ -48,49 +48,52 @@ def prepare_data():
         files = glob.glob(f"{rfp}/*")
         
         for file in files:
-            [main_act,sub_act] = file.split(".csv")[0].split('_')[-2:]
-            # main_activity = file.split(".csv")[0].split('_')[-2]
+            # [main_act,sub_act] = file.split(".csv")[0].split('_')[-2:]
+            main_activity = " ".join(file.split(".csv")[0].split('_')[-2:])
             
             df = pd.read_csv(file,index_col=0)
-            df['main_activity'] = main_act
-            df['sub_activity'] = sub_act
-            df['activity'] = " ".join([main_act,sub_act])
+            df['activity'] = main_activity
             df['user'] = rfp.split('\\')[-1]
             # print(df)
-            df1 = pd.concat([df, df1], axis=0)  
+            df1 = df1.append(df)
 
-    classes = ['lyingBack breathingNormal', 'lyingBack coughing',
-       'lyingBack hyperventilating', 'lyingLeft breathingNormal',
-       'lyingLeft coughing', 'lyingLeft hyperventilating',
-       'lyingRight breathingNormal', 'lyingRight coughing',
-       'lyingRight hyperventilating', 'lyingStomach breathingNormal',
-       'lyingStomach coughing', 'lyingStomach hyperventilating',
-       'sitting breathingNormal', 'sitting coughing',
-       'sitting hyperventilating', 'standing breathingNormal',
-       'standing coughing', 'standing hyperventilating']
-
-
-    df1 = df1[df1['activity'].isin(classes)]   
-    # Combining sitting and standing activities  
-    df1.loc[df1['main_activity'].isin(('sitting', 'standing')),'main_activity'] = 'sitting_standing'
-
-    df1['activity'] = df1[['main_activity', 'sub_activity']].agg(' '.join, axis=1) 
+    df1['activity'] = df1['activity'].apply(lambda x: x.replace('standing','sitting/standing'))
+    df1['activity'] = df1['activity'].apply(lambda x: x.replace('sitting ','sitting/standing '))
     
-    # Selecting required columns only
     columns = ['user','activity','timestamp', 'accel_x', 'accel_y', 'accel_z']
     df_har = df1[columns]
     # removing null values
     df_har = df_har.dropna()
+    df_har.shape
     # transforming the user to float
     df_har['user'] = df_har['user'].str.replace('s', '')
     df_har['user'] = df_har['user'].apply(lambda x:int(x))
-    
-    df_har.to_csv('./t2_data/raw_data.csv',index=False)
+
+    classes = ['lyingBack breathingNormal', 'lyingBack coughing',
+        'lyingBack hyperventilating', 'lyingBack laughing',
+        'lyingBack singing', 'lyingBack talking',
+        'lyingLeft breathingNormal', 'lyingLeft coughing',
+        'lyingLeft hyperventilating', 'lyingLeft laughing',
+        'lyingLeft singing', 'lyingLeft talking',
+        'lyingRight breathingNormal', 'lyingRight coughing',
+        'lyingRight hyperventilating', 'lyingRight laughing',
+        'lyingRight singing', 'lyingRight talking',
+        'lyingStomach breathingNormal', 'lyingStomach coughing',
+        'lyingStomach hyperventilating', 'lyingStomach laughing',
+        'lyingStomach singing', 'lyingStomach talking',
+        'sitting/standing breathingNormal', 'sitting/standing coughing',
+        'sitting/standing eating', 'sitting/standing hyperventilating',
+        'sitting/standing laughing', 'sitting/standing singing',
+        'sitting/standing talking']
+
+
+    df_har = df_har[df_har['activity'].isin(classes)] 
+    df_har.to_csv('./t3_data/raw_data.csv',index=False)
     
 def load_data():
     # ONLY RUN THIS AFTER CSV GENERATION
-    stationary_respiratory_df = pd.read_csv('./t2_data/raw_data.csv')
-    return stationary_respiratory_df
+    all_df = pd.read_csv('./t3_data/raw_data.csv')
+    return all_df
 
 def segments_no_overlap(data):
     segments = []
@@ -113,8 +116,8 @@ def segments_no_overlap(data):
 def get_segment_label(train_df,test_df):
     # saving training and testing data
     user = test_df['user'].unique()[0]
-    train_df.to_csv(f'./t2_data/train/{user}_train.csv')
-    test_df.to_csv(f'./t2_data/test/{user}_test.csv')
+    train_df.to_csv(f'./t3_data/train/{user}_train.csv')
+    test_df.to_csv(f'./t3_data/test/{user}_test.csv')
     
     # segmenting the data into windows 
     train_segments, train_labels = segments_no_overlap(train_df)
@@ -146,14 +149,14 @@ def model_cnn(trainX, trainy):
     
     
 if __name__ == '__main__':
-    # os.mkdir('./t2_data')
-    # os.mkdir('./t2_data/train')
-    # os.mkdir('./t2_data/test')
-    # os.mkdir('./t2_data/models')
+    os.mkdir('./t3_data')
+    os.mkdir('./t3_data/train')
+    os.mkdir('./t3_data/test')
+    os.mkdir('./t3_data/models')
     
     
-    # prepare_data()
-    stationary_respiratory_df = load_data()
+    prepare_data()
+    all_df = load_data()
        
     n_time_steps = 50 
     n_features = 3 
@@ -162,12 +165,12 @@ if __name__ == '__main__':
     batch_size = 32
     
     accuracies = {}
-    for user in stationary_respiratory_df['user'].unique():
+    for user in all_df['user'].unique():
         # if user != 91 :
         #     continue
         
-        train_df = stationary_respiratory_df[stationary_respiratory_df['user'] != user]
-        test_df = stationary_respiratory_df[stationary_respiratory_df['user'] == user]
+        train_df = all_df[all_df['user'] != user]
+        test_df = all_df[all_df['user'] == user]
         
         X_train, y_train, X_test, y_test, categories = get_segment_label(train_df,test_df)
         
@@ -185,11 +188,11 @@ if __name__ == '__main__':
         tflite_model = converter.convert()
 
         # Save the model.
-        with open(f'./t2_data/models/cnn_model_t2_u{user}_{n_time_steps}_{step}_{n_features}.tflite', 'wb') as f:
+        with open(f'./t3_data/models/cnn_model_t3_u{user}_{n_time_steps}_{step}_{n_features}.tflite', 'wb') as f:
             f.write(tflite_model)
         
         # break
 
     a_df = pd.DataFrame(accuracies)
-    a_df.to_csv('./t2_data/t2_loo_accuracies.csv')
+    a_df.to_csv('./t3_data/t3_loo_accuracies.csv')
         
